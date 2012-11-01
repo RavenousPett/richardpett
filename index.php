468e2d79052bfae8049fc50ca3d803522d0c8a02
Richard Pett <rich_pett@hotmail.com>
2012-11-01 21:17:52 +0000

    committing porfolio for the first time, mainly so we can push to pagodabox

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>RichardPett.com - Web Developer</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <style>
+            body {
+                padding-top: 46px;
+                padding-bottom: 40px;
+            }
+        </style>
+
+        <!-- Google Web Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Armata' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="css/styles.css">
+
+        <!-- Nivo Slider -->
+        <link rel="stylesheet" href="css/nivoslider/themes/default/default.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/nivoslider/nivo-slider.css" type="text/css" media="screen" />
+
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner" id="nav-bar">
+                <div class="container">
+                    <a class="brand" href="/">Richard Pett</a>
+                    <p class="strapline"> - Web Develpment Porfolio</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="header">
+
+            <div class="fade-top">
+
+                <div class="container">    
+
+                    <div class="row">
+                        <div class="span8">
+
+                            <div class="slider-wrapper theme-default">
+                                <div id="slider" class="nivoSlider">
+                                    <a href="http://dev7studios.com"><img src="images/up.jpg" data-thumb="images/up.jpg" alt="" title="This is an example of a caption" /></a>
+                                    <img src="images/walle.jpg" data-thumb="images/walle.jpg" alt="" />
+                                    <img src="images/nemo.jpg" data-thumb="images/nemo.jpg" alt="" title="#htmlcaption" />
+                                </div>
+                                <div id="htmlcaption" class="nivo-html-caption">
+                                    <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>. 
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="span4">
+                            <h1>My Work</h1>
+                            <p class="bold">This site is all about getting to know me and my work. So get stuck in and discover what
+                                I can do!</p>
+                            <p>I'm a web developer with a passion for clean and robust code. I love creating things, problem solving and the satisfaction
+                                that comes from learning and applying new technologies. </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div><!-- end of fade top-->
+
+        </div>
+
+        <div class="shadow-bottom">  </div><!-- end of shadow top-->
+
+        <div id="skills">
+
+            <div class="container">    
+
+                <div class="row">
+                    <div class="span4">
+                        <h1>Skill Set</h1>
+                        <p class="bold">This site is all about getting to know me and my work. So get stuck in and discover what
+                            I can do!</p>
+                        <p>I'm a web developer with a passion for clean and robust code. I love creating things, problem solving and the satisfaction
+                            that comes from learning and applying new technologies. </p>
+                    </div>
+                    <div class="span8">
+
+                    <div id="htmltagcloud"> <span id="0" class="wrd tagcloud6"><a>ajax</a></span> <span id="1" class="wrd tagcloud0"><a>apache</a></span> <span id="2" class="wrd tagcloud6"><a>codeigniter</a></span> <span id="3" class="wrd tagcloud10"><a>css</a></span> <span id="4" class="wrd tagcloud6"><a>css3</a></span> <span id="5" class="wrd tagcloud3"><a>git</a></span> <span id="6" class="wrd tagcloud10"><a>html</a></span> <span id="7" class="wrd tagcloud4"><a>html5</a></span> <span id="8" class="wrd tagcloud0"><a>iis</a></span> <span id="9" class="wrd tagcloud10"><a>javascript</a></span> <span id="10" class="wrd tagcloud6"><a>joomla</a></span> <span id="11" class="wrd tagcloud10"><a>jquery</a></span> <span id="12" class="wrd tagcloud6"><a>json</a></span> <span id="13" class="wrd tagcloud3"><a>linux</a></span> <span id="14" class="wrd tagcloud6"><a>mvc</a></span> <span id="15" class="wrd tagcloud6"><a>mysql</a></span> <span id="16" class="wrd tagcloud6"><a>oop</a></span> <span id="17" class="wrd tagcloud7"><a>photoshop</a></span> <span id="18" class="wrd tagcloud10"><a>php</a></span> <span id="19" class="wrd tagcloud3"><a>rest</a></span> <span id="20" class="wrd tagcloud6"><a>seo</a></span> <span id="21" class="wrd tagcloud0"><a>soap</a></span> <span id="22" class="wrd tagcloud6"><a>svn</a></span> <span id="23" class="wrd tagcloud7"><a>wordpress</a></span> </div>
+                    <!-- end tag cloud : generated by TagCrowd.com : please keep this notice -->
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="container" id="projects">
+
+            <div class="row">
+                <div class="span12">
+                    <h1>Projects</h1>
+                </div>
+            </div>
+
+            <!-- Example row of columns -->
+            <div class="row">
+
+                <div class="span4">
+                    <div class="thumb-wrap">
+                        <img class="thumb-corner" src="images/thumb-corner.png" />
+                        <img class="thumb-pic" src="projects/intense/thumb.jpg" />
+                    </div>
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View project &raquo;</a></p>
+                </div>
+                <div class="span4">
+                    <div class="thumb-wrap">
+                        <img class="thumb-corner" src="images/thumb-corner.png" />
+                        <img class="thumb-pic" src="projects/intense/thumb.jpg" />
+                    </div>
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View project &raquo;</a></p>
+               </div>
+                <div class="span4">
+                    <div class="thumb-wrap">
+                        <img class="thumb-corner" src="images/thumb-corner.png" />
+                        <img class="thumb-pic" src="projects/intense/thumb.jpg" />
+                    </div>
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a class="btn" href="#">View project &raquo;</a></p>
+                </div>
+            </div>
+
+            <hr>
+
+            <footer>
+                <p>&copy; Company 2012</p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+        <script type="text/javascript">
+            $(window).load(function() {
+                $('#slider').nivoSlider(
+                    {
+                    // boxCols: 10, // For box animations
+                    // boxRows: 10,
+                    effect: 'sliceDownRight',
+                    pauseTime: 4500,
+                    }
+                );
+            });
+        </script>
+
+        <script src="js/main.js"></script>
+
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
